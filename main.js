e16cc9df1c99106cf5c3162ba5f52b7e44094532
matincodes <abdulmatinadeniji@gmail.com>
2022-05-05 18:54:22 +0100

    adding new files

---
+++ main.js
@@ -0,0 +1,10 @@
+let menu =  document.querySelector(".menu");
+let nav = document.querySelector('.header__nav ul');
+
+
+
+menu.addEventListener('click', function(){
+     nav.classList.toggle("show")
+
+})
+
